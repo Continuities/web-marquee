bdcc0225a106746753bde99aa96281eee6f71f17
Michael Townsend <michael@doublespeakgames.com>
2020-03-24 17:56:41 -0400

    Added ui

--- server/index.js
+++ server/index.js
@@ -6,9 +6,10 @@ const app = express();
 const port = 3000;
 const serial = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
 
+app.use(express.static('public'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send(`I'm a server.`));
 app.post('/', (req, res) => {
   console.log(`RX: ${req.body.message}`);
   serial.write(`${req.body.message}\n`);
