351421f5e14c3c290ee65293a3a6f1693c87dbac
Michael Townsend <michael@doublespeakgames.com>
2020-03-27 10:57:05 -0400

    No more depracated marquee

--- server/public/main.js
+++ server/public/main.js
@@ -1,18 +1,33 @@
 import ReconnectingWebSocket from './reconnecting-socket.js';
 
+const SCROLL_RATE = 1 / 100; // pixels per second
+
 const socket = ReconnectingWebSocket(`ws://${window.location.host}`);
 socket.onmessage = e => {
   const [ colour, message ] = e.data.split('|');
-  if (display.innerText != message) {
-    display.innerText = message;
-  }
+  updateMessage(message);
   display.style.color = `#${colour}`;
 };
 
 const form = document.getElementById('content');
 const input = document.getElementById('input');
-const display = document.getElementById('display');
+const display = document.getElementById('inner-display');
 const colour = document.getElementById('colour');
+
+function updateMessage(newMessage) {
+  if (display.innerText === newMessage) {
+    return;
+  }
+  display.style.animation = 'none';
+  display.offsetHeight; /* trigger reflow */
+  display.style.animation = null; 
+  display.innerText = newMessage;
+  const width = display.clientWidth;
+  const scrollDuration = SCROLL_RATE * width;
+  display.style.animationDuration = `${scrollDuration}s`;
+}
+
+window.update = updateMessage;
 
 function updateColour() {
   input.style.color = colour.value;
